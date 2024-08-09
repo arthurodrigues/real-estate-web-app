37a994201f4e56cdac4a83ff69b6110ed2cd1fd2
Arthur Rodrigues <arthuraguiar6380@gmail.com>
2024-08-09 14:12:03 -0300

    create test api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,16 +8,14 @@
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
-
-    </Routes>
-    
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
   )
 }
