1688eba9f60a4f3545f65fd3ea1b019f4f0e3861
Arthur Rodrigues <arthuraguiar6380@gmail.com>
2024-08-21 21:13:14 -0300

    complete sing in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 
 export default function SignUp() {
-  const [formData, setFormData] = useState({})
+  const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
