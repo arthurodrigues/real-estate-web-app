5704ad6fe0bf1d663b40aba6b8c38107163933ac
Arthur Rodrigues <arthuraguiar6380@gmail.com>
2024-08-22 23:02:47 -0300

    add redux toolkit and some issues fixes

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -26,7 +26,7 @@ export default function SignUp() {
       });
       const data = await res.json();
       console.log(data);
-      if(data.sucess === false) {
+      if(data.success === false) {
         setLoading(false);
         setError(data.message);
         return;
