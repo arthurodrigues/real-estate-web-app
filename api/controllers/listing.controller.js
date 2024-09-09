2c488338eddd3643895783ca5a557199da8023e3
Arthur Rodrigues <arthuraguiar6380@gmail.com>
2024-09-09 15:10:18 -0300

    complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,14 +1,35 @@
 import mongoose from 'mongoose';
 import Listing from "../models/listing.model.js";
+import { errorHandler } from '../utils/error.js';
 
 export const createListing = async (req, res, next) => {
     try {
+        if (!mongoose.isValidObjectId(req.user.id)) {
+            return next(errorHandler(400, 'Invalid user ID format'));
+        }
         const listingData = {
             ...req.body,
-            userRef: new mongoose.Types.ObjectId(req.user.id),
+            userRef: req.user.id,
         };
         const listing = await Listing.create(listingData);
         return res.status(201).json(listing);
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing){
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+    if(req.user.id !== listing.userRef.toString()){
+        return next(errorHandler(401, 'You can only delete your own listing!'));
+    }
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json('Listing has been deleted!');
     } catch (error) {
         next(error);
     }
